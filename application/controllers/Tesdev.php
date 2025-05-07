777d7ce506e792a1468de8d4abe8950e950bec71
jangudin <121915901+jangudin@users.noreply.github.com>
2025-05-07 09:18:57 -0700

    Update Tesdev.php

--- application/controllers/Tesdev.php
+++ application/controllers/Tesdev.php
@@ -7,7 +7,7 @@ class Tesdev extends CI_Controller {
         date_default_timezone_set('Asia/Jakarta');
         ini_set('max_execution_time', '300');
         $this->load->model('Dashboard_tte');
-        $this->load->library('encrypt');
+        $this->load->library('encryption');
         $this->load->helper('tanggal_indonesia');
     }
 
