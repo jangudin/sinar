0d7847cdef2a3aacb19436c34543dbe9f8e2e5cc
jangudin <121915901+jangudin@users.noreply.github.com>
2025-05-08 04:35:29 +0000

    view jumlah tpmd

--- application/views/adminsurattpmd.php
+++ application/views/adminsurattpmd.php
@@ -11,7 +11,9 @@
     <div class="col-md-12 ">
       <div class="x_panel">
         <div class="x_title">
-          <h2>Jumlah belum input nomor : <?php echo $belum->belum; ?> </h2>
+          <h2>Jumlah belum input nomor : <?php 
+        echo (isset($belum) && !is_null($belum) && isset($belum->belum)) ? $belum->belum : 0; 
+    ?> </h2>
           <ul class="nav navbar-right panel_toolbox">
             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
             </li>
