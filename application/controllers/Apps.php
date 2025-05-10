60a0ce93b39b4f80968a1c99d5b8b239e81b9639
wahyu din <jangudin12345@gmail.com>
2025-05-10 12:09:08 -0700

    tutup menu TPMD sementara

--- application/controllers/Apps.php
+++ application/controllers/Apps.php
@@ -5,7 +5,7 @@ class Apps extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('User_model');
+        $this->load->model('ModelApps'); // Load the correct model
         if($this->session->userdata('status') != "login"){
             redirect(base_url());
         }
@@ -13,8 +13,8 @@ class Apps extends CI_Controller {
 
     public function index() {
         $data['title'] = 'Sinar || Dashboard';
-        $user_id = $this->session->userdata('name');
-        $data['menu'] = $this->User_model->getMenuAndSubMenuByUser($user_id);
+        $user_id = $this->session->userdata('id'); // Assuming 'id' is the user ID stored in session
+        $data['menu'] = $this->ModelApps->getMenuAndSubMenuByUser ($user_id); // Call the method from ModelApps
         $this->load->view('template_dashboard/dashboard', $data);
     }
 }
---
+++ application/models/ModelApps.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ModelApps extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get menu and submenu data based on user ID
+     * 
+     * @param int $user_id User's ID (to filter access)
+     * @return array Menu and submenu accessible by the user
+     */
+    public function getMenuAndSubMenuByUser($user_id)
+{
+    $this->db->select('apps_menu.nama_menu, apps_menu.icon, apps_sub_menu.nama_sub_menu');
+    $this->db->from('apps_role_akses');
+    $this->db->join('apps_menu', 'apps_role_akses.id_apps_menu = apps_menu.id', 'left');
+    $this->db->join('apps_sub_menu', 'apps_role_akses.id_apps_sub_menu = apps_sub_menu.id', 'left');
+    $this->db->join('apps_group_akses_menu', 'apps_role_akses.id_apps_group_akses = apps_group_akses_menu.id', 'left');
+    $this->db->where('apps_role_akses.id_pengguna', $user_id);
+
+    $query = $this->db->get();
+    return $query->result_array();
+}
+
+}
