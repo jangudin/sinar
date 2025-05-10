b9c2dfbd2caf84f893b95020c7a947086b05c3bb
wahyu din <jangudin12345@gmail.com>
2025-05-10 12:57:06 -0700

    tutup menu TPMD sementara

--- application/views/template_dashboard/dashboard.php
+++ application/views/template_dashboard/dashboard.php
@@ -485,7 +485,7 @@
 
     <div>
 <?php foreach ($menu_data as $menu): ?>
-  <div class="nav-item" data-section="<?php echo htmlspecialchars($menu['section']); ?>" tabindex="0" role="button" aria-pressed="false">
+  <div class="nav-item" data-section="<?php echo htmlspecialchars($menu['icon']); ?>" tabindex="0" role="button" aria-pressed="false">
     <i class="<?php echo htmlspecialchars($menu['icon']); ?>" aria-hidden="true"></i>
     <span class="label"><?php echo htmlspecialchars($menu['nama_menu']); ?></span>
   </div>
