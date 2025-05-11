5f91a6f353f1b232bf1a90ca10df120359c05bf3
wahyu din <jangudin12345@gmail.com>
2025-05-11 01:23:30 -0700

    tutup menu TPMD sementara

--- application/libraries/Pdfgenerator.php
+++ application/libraries/Pdfgenerator.php
@@ -74,21 +74,73 @@ class Pdfgenerator {
         // }
     }
 
-     public function generatefaskes($html, $filename='', $paper = '', $orientation = '', $stream=TRUE)
-    {   
-        $options = new Options();
-        $options->set('isRemoteEnabled', TRUE);
-        $dompdf = new Dompdf($options);
-        $dompdf->loadHtml($html);
-        $dompdf->setPaper($paper, $orientation);
-        $dompdf->render();
-        $outPut = $dompdf->output();
-        file_put_contents('assets/faskessertif/'.$filename.'.pdf',$outPut);
-        // if ($stream) {
-        //     $dompdf->stream($filename.".pdf", array("Attachment" => 0));
-        // } else {
-        //     return $dompdf->output();
-        // }
+    //  public function generatefaskes($html, $filename='', $paper = '', $orientation = '', $stream=TRUE)
+    // {   
+    //     $options = new Options();
+    //     $options->set('isRemoteEnabled', TRUE);
+    //     $dompdf = new Dompdf($options);
+    //     $dompdf->loadHtml($html);
+    //     $dompdf->setPaper($paper, $orientation);
+    //     $dompdf->render();
+    //     $outPut = $dompdf->output();
+    //     file_put_contents('assets/faskessertif/'.$filename.'.pdf',$outPut);
+    // }
+
+    public function generatefaskes($html, $filename = '', $paper = '', $orientation = '', $stream = TRUE)
+{   
+    // Define options for DOMPDF
+    $options = new Options();
+    $options->set('isRemoteEnabled', TRUE);  // Enable remote resources (images from URLs)
+    $options->set('isHtml5ParserEnabled', TRUE); // Enable HTML5 parser for modern HTML content
+    $dompdf = new Dompdf($options);
+
+    // Replace local image paths with absolute URLs if needed
+    $html = $this->replaceLocalImagePaths($html);
+
+    // Load HTML content
+    $dompdf->loadHtml($html);
+
+    // Set paper size and orientation
+    $dompdf->setPaper($paper, $orientation);
+
+    // Render the PDF (first pass)
+    $dompdf->render();
+
+    // Get the PDF output
+    $output = $dompdf->output();
+
+    // Define file path to save the generated PDF
+    $filePath = 'assets/faskessertif/' . $filename . '.pdf';
+
+    // Ensure the directory exists and is writable
+    if (!is_dir('assets/faskessertif/')) {
+        mkdir('assets/faskessertif/', 0777, true);  // Create the directory if it doesn't exist
+    }
+    if (!is_writable('assets/faskessertif/')) {
+        throw new Exception("Directory is not writable: assets/faskessertif/");
+    }
+
+    // Save the generated PDF to the file system
+    file_put_contents($filePath, $output);
+
+    // If $stream is TRUE, stream the PDF to the browser
+    if ($stream) {
+        // Stream the generated PDF to the browser
+        $dompdf->stream($filename . ".pdf", array("Attachment" => 0));  // 0: Display in browser, 1: Force download
+    } else {
+        // Return the raw PDF output if needed elsewhere
+        return $output;
+    }
+}
+
+    private function replaceLocalImagePaths($html)
+    {
+        $baseUrl = 'http://192.168.67.143';  // Use the internal server base URL
+        $html = preg_replace_callback('/src=["\'](\/sinar\/assets\/faskesbg\/[^\s]+)["\']/i', function($matches) use ($baseUrl) {
+            return 'src="' . $baseUrl . $matches[1] . '"';  // Convert relative path to absolute URL
+        }, $html);
+
+        return $html;
     }
 
          public function generatefaskesdirjen($html, $filename='', $paper = '', $orientation = '', $stream=TRUE)
