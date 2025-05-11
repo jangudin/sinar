0db7b7edbf394ff2e7b6392edefc2b706b8dc2c2
wahyu din <jangudin12345@gmail.com>
2025-05-11 02:40:58 -0700

    tutup menu TPMD sementara

--- application/controllers/Tesdev.php
+++ application/controllers/Tesdev.php
@@ -2,38 +2,134 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tesdev extends CI_Controller {
-
-    public function __construct()
-    {
+    function __construct(){
         parent::__construct();
-        // Memuat TCPDF library
-        $this->load->library('tcpdf');
+        date_default_timezone_set('Asia/Jakarta');
+        ini_set('max_execution_time', '300');
+        $this->load->model('Dashboard_tte');
+        $this->load->library('encryption');
+        $this->load->helper('tanggal_indonesia');
     }
 
-    public function generate_pdf()
+    // public function index()
+    // {
+
+    //     $this->file1();
+    //     $this->file2();
+
+        
+    // }
+
+
+        public function file1()
     {
-        // Membuat instance dari TCPDF
-        $pdf = new TCPDF();
-        
-        // Set beberapa parameter untuk PDF
-        $pdf->SetCreator(PDF_CREATOR);
-        $pdf->SetAuthor('Your Name');
-        $pdf->SetTitle('Sample PDF using TCPDF');
-        $pdf->SetSubject('TCPDF Example');
-        
-        // Menambahkan halaman baru
-        $pdf->AddPage();
 
-        // Mengatur font
-        $pdf->SetFont('helvetica', '', 12);
+        $this->load->library('pdfgenerator');
+        $this->data['title_pdf'] = 'Sertifikat';
+        $file_pdf = "Lafki11";
+        $paper = 'A4';
+        $orientation = "landscape";
+        $html =  $this->load->view('Sertifikatfaskesnew/sertifikatkosong',$this->data,true);
+        $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+
+         //   $this->load->view('Sertifikatfaskesnew/sertifikatkosong');
+
         
-        // Menulis teks ke dalam PDF
-        $pdf->Cell(0, 10, 'Hello, this is a simple PDF generated using TCPDF in CodeIgniter.', 0, 1, 'C');
-        
-        // Menambahkan gambar (URL eksternal atau file lokal)
-        $pdf->Image('http://192.168.67.143/sinar/assets/faskesbg/backgroundsertifikat.jpeg', 10, 20, 50, 50, 'jpeg');
-        
-        // Output PDF ke browser
-        $pdf->Output('example.pdf', 'I');
     }
-}
+
+        public function file2()
+    {
+
+        $this->load->library('pdfgenerator');
+        $this->data['title_pdf'] = 'Sertifikat';
+        $file_pdf = "Lafki32";
+        $paper = 'A4';
+        $orientation = "landscape";
+        $html =  $this->load->view('tespage',$this->data,true);
+
+
+        $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+
+            //$this->load->view('tespage');
+
+        
+    }
+
+
+    // function Folder()
+    // {
+    //         $path    = './assets/faskessertif'; //lokasi folder sekarang 
+    //         $files = scandir($path);
+    //         $files = array_diff(scandir($path), array('.', '..'));
+    //         foreach($files as $nama_file)
+    //         {
+    //             echo "<a href='$nama_file'>$nama_file</a><br/>";
+    //             echo  "<hr>";
+    //         }
+    //     }
+
+    // public function index()
+    // {
+    //     // panggil library yang kita buat sebelumnya yang bernama pdfgenerator
+    //     $this->load->library('pdfgenerator');
+        
+    //     // title dari pdf
+    //     $this->data['title_pdf'] = 'tes';
+        
+    //     // filename dari pdf ketika didownload
+    //     $file_pdf = 'tes';
+    //     // setting paper
+    //     $paper = 'A4';
+    //     //orientasi paper potrait / landscape
+    //     $orientation = "landscape";
+        
+    //     $html = $this->load->view('tespage',$this->data, true);     
+        
+    //     // run dompdf
+    //     $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+
+    //     // $this->load->view('tespage');
+    // }
+
+ //        public function index()
+ // {
+ //            $this->data['title_pdf'] = 'tes';
+ //        $this->load->library('pdfgenerator');
+ //        $file_pdf = 'tes';
+ //        $paper = 'A4';
+ //        $orientation = "landscape";
+ //        $html = $this->load->view('tespage',$this->data, true);     
+ //        $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+        
+ //    }
+
+ //            public function index()
+ // {
+ //        $this->data['title_pdf'] = 'tes';
+ //        $this->load->library('pdfgenerator');
+ //        $file_pdf = 'tes';
+ //        $paper = 'A4';
+ //        $orientation = "potrait";
+ //        $html = $this->load->view('surtug/surat',$this->data, true);     
+ //        $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+        
+ //    }
+
+
+ //                public function index()
+ // {
+ //        // $this->data['title_pdf'] = 'tes';
+ //        // $this->load->library('pdfgenerator');
+ //        // $file_pdf = 'tes';
+ //        // $paper = 'A4';
+ //        // $orientation = "potrait";
+ //        // $html = $this->load->view('surtug/surat',$this->data, true);     
+ //        // $this->pdfgenerator->generatetes($html, $file_pdf,$paper,$orientation);
+
+ //     echo "Sedang Ada Pemeliharaan Kembali Beberapa Saat lagi";
+        
+ //    }
+
+
+
+    }
