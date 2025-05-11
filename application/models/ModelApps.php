ad2e1efe7bbcfd55fe365a88e58da448ff9bf62e
wahyu din <jangudin12345@gmail.com>
2025-05-10 21:47:03 -0700

    tutup menu TPMD sementara

--- application/models/ModelApps.php
+++ application/models/ModelApps.php
@@ -28,4 +28,19 @@ class ModelApps extends CI_Model {
         $query = $this->db->get();
         return $query->result_array();
     }
+
+    public function getSubMenu($user_id)
+    {
+        $this->db->select('apps_menu.nama_menu, apps_menu.icon, apps_sub_menu.nama_sub_menu');
+        $this->db->from('apps_role_akses');
+        $this->db->join('apps_menu', 'apps_role_akses.id_apps_menu = apps_menu.id', 'left');
+        $this->db->join('apps_sub_menu', 'apps_role_akses.id_apps_sub_menu = apps_sub_menu.id', 'left');
+        $this->db->join('apps_group_akses_menu', 'apps_role_akses.id_apps_group_akses = apps_group_akses_menu.id', 'left');
+        $this->db->where('apps_role_akses.id_user', $user_id);
+
+        $this->db->group_by('apps_menu.id');
+
+        $query = $this->db->get();
+        return $query->result_array();
+    }
 }
