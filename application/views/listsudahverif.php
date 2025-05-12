17537a7d71e9c3d8ed64f9e050e401d900edb138
wahyu din <jangudin12345@gmail.com>
2025-05-12 06:50:57 -0700

    update sertifikat kosong

--- application/views/listsudahverif.php
+++ application/views/listsudahverif.php
@@ -53,7 +53,7 @@
                     <?php endif ?>
                 </td>
                 <td>
-                  <a class="btn btn-sm btn-success" href="<?php echo base_url('Mutu_fasyankes/Detail/'.$this->encryption->encrypt($a->id));?>"><span data-feather="edit"></span> Review</a>
+                  <a class="btn btn-sm btn-success" href="<?php echo base_url('Mutu_fasyankes/Detail/');?><?php if (!is_null($item->id)): ?><?= encrypt($item->id) ?><?php else: ?><?php endif; ?>"><span data-feather="edit"></span> Review</a>
                   
                 </td>
                 </tr>
