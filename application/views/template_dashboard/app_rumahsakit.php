8984e0a34456b291a10612e7a7385e2a844ee7f4
wahyu din <jangudin12345@gmail.com>
2025-05-13 10:16:54 -0700

    tutup menu TPMD sementara

--- application/views/template_dashboard/app_rumahsakit.php
+++ application/views/template_dashboard/app_rumahsakit.php
@@ -1,11 +1,11 @@
 <?php if ($this->session->userdata('apps') == '5'): ?>
 <div class="files-grid" role="list" aria-live="polite" aria-relevant="all">
         <div class="file-item" data-href="apps/verifikasirs" role="listitem" tabindex="0" aria-label="Folder: Projects" style="cursor: pointer;">
-          <span class="material-icons file-icon" style="color:#fbbc04;">folder</span>
+          <span class="material-icons file-icon" style="color:#fbbc04;">verified</span>
           <div class="file-name">Verifikasi</div>
         </div>
         <div class="file-item" data-href="apps/tters" role="listitem" tabindex="0" aria-label="Folder: Projects" style="cursor: pointer;">
-          <span class="material-icons file-icon" style="color:#34a853;">folder</span>
+          <span class="material-icons file-icon" style="color:#34a853;">signature</span>
           <div class="file-name">TandaTangan</div>
         </div> 
       </div>
