1a6f049d5b0a58a905f3e89b1a5e7bd67bffc0ce
wahyu din <jangudin12345@gmail.com>
2025-05-13 10:13:54 -0700

    tutup menu TPMD sementara

--- application/views/template_dashboard/app_rumahsakit.php
+++ application/views/template_dashboard/app_rumahsakit.php
@@ -2,7 +2,7 @@
 <div class="files-grid" role="list" aria-live="polite" aria-relevant="all">
         <div class="file-item" data-href="apps/verifikasirs" role="listitem" tabindex="0" aria-label="Folder: Projects" style="cursor: pointer;">
           <span class="material-icons file-icon" style="color:#fbbc04;">folder</span>
-          <div class="file-name">RS</div>
+          <div class="file-name">Verifikasi</div>
         </div>
         <div class="file-item" data-href="apps/tters" role="listitem" tabindex="0" aria-label="Folder: Projects" style="cursor: pointer;">
           <span class="material-icons file-icon" style="color:#34a853;">folder</span>
