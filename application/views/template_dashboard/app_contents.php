2b57ced3950a868cd49382c82c54773298adee8f
wahyu din <jangudin12345@gmail.com>
2025-05-13 09:35:23 -0700

    ganti user pass sinar tte

--- application/views/template_dashboard/app_contents.php
+++ application/views/template_dashboard/app_contents.php
@@ -1,6 +1,6 @@
 <?php if ($this->session->userdata('apps') == '5'): ?>
 <div class="files-grid" role="list" aria-live="polite" aria-relevant="all">
-        <div class="file-item" role="listitem" tabindex="0" aria-label="Folder: Projects">
+        <div class="file-item" data-href="your-link-here" role="listitem" tabindex="0" aria-label="Folder: Projects" style="cursor: pointer;">
           <span class="material-icons file-icon" style="color:#fbbc04;">folder</span>
           <div class="file-name">RS</div>
         </div>
@@ -36,3 +36,11 @@
         </div>
       </div>
 <?php endif; ?>
+<script>
+document.querySelectorAll('.file-item').forEach(item => {
+  item.addEventListener('click', () => {
+    const href = item.dataset.href;
+    if (href) window.location.href = href;
+  });
+});
+</script>
