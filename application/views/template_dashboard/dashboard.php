f58c077aad3b9cef4ddb4c4a11d228cddd1132fc
wahyu din <jangudin12345@gmail.com>
2025-05-13 03:09:29 -0700

    ganti user pass sinar tte

--- application/views/template_dashboard/dashboard.php
+++ application/views/template_dashboard/dashboard.php
@@ -470,7 +470,7 @@
     <section class="files-container" aria-label="Files and folders">
       <!-- Grid View -->
 
-        <?php $this->load->view('template_dashboard/contents'); ?>
+        <?php $this->load->view('template_dashboard/app_contents'); ?>
         
         <!-- Table View (hidden by default) -->
       <div class="table-responsive" aria-label="File table responsive container">
