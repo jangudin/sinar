1538616b44c48ecc718b60a5c0fd5fe056a047ff
wahyu din <jangudin12345@gmail.com>
2025-05-13 18:27:22 -0700

    ganti user pass sinar tte

--- application/controllers/Tesdev.php
+++ application/controllers/Tesdev.php
@@ -41,4 +41,45 @@ class Tesdev extends CI_Controller {
         // Tutup koneksi cURL
         curl_close($ch);
     }
+
+     public function login_faskes()
+    {
+        $url = 'https://api-yankes.kemkes.go.id/faskes/login';
+        $postData = json_encode([
+            'userName' => 'kotakelektronik@gmail.com',
+            'password' => '123'
+        ]);
+
+        $headers = [
+            'Content-Type: application/json',
+            'Cookie: refreshToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwidXNlcl9uYW1lIjoia290YWtlbGVrdHJvbmlrQGdtYWlsLmNvbSIsImxldmVsX2lkIjoyLCJpYXQiOjE3NDcxODU5MTQsImV4cCI6MTc0NzI3MjMxNH0.AzYmuBjdC4EQp6dDCVfHKwLiyPhjXSzuBuCuY9NNcQM'
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postData,
+            CURLOPT_HTTPHEADER => $headers,
+        ));
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            echo 'Curl Error: ' . curl_error($ch);
+        } else {
+            echo "<pre>";
+            var_dump(json_decode($response, true));
+            echo "</pre>";
+        }
+
+        curl_close($ch);
+    }
     }
