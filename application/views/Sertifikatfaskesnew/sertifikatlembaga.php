2d3263d29a5aa0facaed734d61e75b8e441ca53e
wahyu din <jangudin12345@gmail.com>
2025-05-13 19:18:57 -0700

    ganti user pass sinar tte

--- application/views/Sertifikatfaskesnew/sertifikatlembaga.php
+++ application/views/Sertifikatfaskesnew/sertifikatlembaga.php
@@ -177,7 +177,7 @@
 <body class="text-centre">
 
   <div id="watermark">
-    <img src="assets/faskesbg/backgroundsertifikat.jpeg" height="100%" width="100%" />
+    <img src="https://sinar.kemkes.go.id/assets/faskesbg/backgroundsertifikat.jpeg" height="100%" width="100%" />
   </div>
   <?php foreach ($data as $s) { ?>
     <main>
@@ -232,13 +232,13 @@
   </div>
   <div class="capayan">
     <?php if ($s->status_akreditasi == 'Paripurna'): ?>
-      <img src="assets/faskessertif/capayan/paripurna.png" height=60 class="center">
+      <img src="https://sinar.kemkes.go.id/assets/faskessertif/capayan/paripurna.png" height=60 class="center">
     <?php elseif ($s->status_akreditasi == 'Utama'): ?>
-      <img src="assets/faskessertif/capayan/utama.png" height=60 class="center">
+      <img src="https://sinar.kemkes.go.id/assets/faskessertif/capayan/utama.png" height=60 class="center">
     <?php elseif ($s->status_akreditasi == 'Madya'): ?>
-      <img src="assets/faskessertif/capayan/madya.png" height=60 class="center">
+      <img src="https://sinar.kemkes.go.id/assets/faskessertif/capayan/madya.png" height=60 class="center">
     <?php elseif ($s->status_akreditasi == 'Dasar'): ?>
-      <img src="assets/faskessertif/capayan/dasar.png" height=60 class="center">
+      <img src="https://sinar.kemkes.go.id/assets/faskessertif/capayan/dasar.png" height=60 class="center">
     <?php endif; ?>
   </div>
   <div class="berlaku">
