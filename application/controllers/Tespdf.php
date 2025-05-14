860a12dd600118c2767f07571bd4d8dd0edbfff4
wahyu din <jangudin12345@gmail.com>
2025-05-14 00:28:05 -0700

    tutup menu TPMD sementara

--- application/controllers/Tespdf.php
+++ application/controllers/Tespdf.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Testhtml extends CI_Controller {
+class Testpdf extends CI_Controller {
     function __construct(){
         parent::__construct();
         date_default_timezone_set('Asia/Jakarta');
