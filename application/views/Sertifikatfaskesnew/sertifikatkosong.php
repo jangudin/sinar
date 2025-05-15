11f1bd41da2fbde47fc35bcc39eb68a201a5459e
wahyu din <jangudin12345@gmail.com>
2025-05-14 21:23:38 -0700

    ganti user pass sinar tte

--- application/views/Sertifikatfaskesnew/sertifikatkosong.php
+++ application/views/Sertifikatfaskesnew/sertifikatkosong.php
@@ -2,26 +2,35 @@
 <html lang="en">
 
 <head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <title>Sertifikat</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Germania+One&display=swap" rel="stylesheet">
   <style>
+    .nmrs {
+      font-family: 'Germania One', cursive;
+    }
+
     @page {
-      margin: 0cm;
+      margin: 0cm 0cm;
     }
 
     body {
-      font-family: 'Arial', sans-serif;
       margin-top: 180px;
       margin-bottom: 0cm;
       margin-left: 0cm;
       margin-right: 0cm;
-      position: relative;
     }
 
     #watermark {
       position: fixed;
-      bottom: 0;
-      left: 0;
+      bottom: 0px;
+      left: 0px;
       width: 29.7cm;
       height: 21cm;
       z-index: -1000;
@@ -36,6 +45,7 @@
     .capayanimgutama,
     .ttdlembaga,
     .ttddirjen,
+    .bsd,
     .berlaku,
     .tglprn,
     .tglsertifikat,
@@ -43,45 +53,47 @@
     .atasnama {
       position: fixed;
       margin: auto;
-      text-align: center;
     }
 
     .sertifikat-nomor {
       top: 25%;
-      width: 100%;
+      text-align: center;
     }
 
     .sertifikat-nama {
       top: 30%;
       left: 14%;
+      text-align: center;
       width: 800px;
     }
 
     .sertifikat-garis {
       top: 33%;
       left: 14%;
+      text-align: center;
       width: 800px;
     }
 
     .sertifikat-tingkat {
       top: 46%;
-      width: 100%;
+      text-align: center;
     }
 
     .capayan {
       top: 60%;
-      width: 100%;
+      text-align: center;
     }
 
     .berlaku {
       top: 67%;
-      width: 100%;
+      text-align: center;
     }
 
     .capayanimgparipurna,
     .capayanimgutama {
       top: 52%;
       left: 25%;
+      position: fixed;
     }
 
     .ttdlembaga {
@@ -94,9 +106,22 @@
       left: 55%;
     }
 
+    .tglprn,
+    .tglsertifikat,
+    .tglsekarang,
+    .atasnama {
+      position: fixed;
+      margin: auto;
+    }
+
+    .title,
+    .desc,
+    .tingkat {
+      font-family: 'bernard-mt-condensed-regular', sans-serif;
+    }
+
     .title {
       font-size: 17px;
-      font-weight: bold;
     }
 
     .desc {
@@ -107,55 +132,107 @@
       font-size: 30px;
       font-weight: bold;
     }
+
+    /* Responsiveness */
+    @media (max-width: 768px) {
+      .sertifikat-nama {
+        width: 90%;
+        left: 5%;
+      }
+
+      .sertifikat-garis {
+        width: 90%;
+        left: 5%;
+      }
+
+      .capayanimgparipurna,
+      .capayanimgutama {
+        top: 55%;
+        left: 15%;
+      }
+
+      .ttdlembaga,
+      .ttddirjen {
+        top: 85%;
+      }
+    }
   </style>
 </head>
 
-<body>
+<body class="text-center">
+
   <div id="watermark">
     <img src="<?= $background_base64 ?>" width="100%" height="100%" />
   </div>
 
   <?php foreach ($data as $s) { ?>
-    <div class="sertifikat-nomor">
-      <p style="font-size: 20px; color: red;">Nomor : <?= htmlspecialchars($s->nomor_sertifikat ?? '123/ABC/2024') ?></p>
-    </div>
+    <main>
+      <div class="sertifikat-nomor">
+        <p style="font-size: 20px; color: red ;">Nomor : </p>
+      </div>
+      <table style="padding-left: 145px; padding-top:80px;">
+        <thead>
+          <tr>
+            <td>
+              <?php if ($s->jenis_fasyankes_nama == 'Pusat Kesehatan Masyarakat'): ?>
+                Puskesmas
+              <?php elseif ($s->jenis_fasyankes_nama == 'Klinik'): ?>
+                Klinik
+              <?php elseif ($s->jenis_fasyankes_nama == 'Laboratorium Kesehatan'): ?>
+                Labkes
+              <?php elseif ($s->jenis_fasyankes_nama == 'Unit Transfusi Darah'): ?>
+                UTD
+              <?php endif; ?>
+            </td>
+            <td>:</td>
+            <th style="font-size: 18px;"><?= $s->nama_fasyankes; ?></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alamat </td>
+            <td>:</td>
+            <td width="700px"><?= $s->alamat; ?></td>
+          </tr>
+          <tr>
+            <td>Kecamatan</td>
+            <td>:</td>
+            <td><?= ucwords(strtolower($s->nama_camat)); ?></td>
+          </tr>
+          <tr>
+            <td>Kabupaten / Kota </td>
+            <td> : </td>
+            <td><?= ucwords(strtolower($s->nama_kota)); ?></td>
+          </tr>
+          <tr>
+            <td>Provinsi </td>
+            <td>:</td>
+            <td><?= $s->nama_prop; ?></td>
+          </tr>
+        </tbody>
+      </table>
 
-    <div class="sertifikat-nama title">
-      <?= htmlspecialchars($s->nama_fasyankes) ?>
-    </div>
+      <div class="bsd">
+        <p class="desc">sebagai pengakuan bahwa Fasilitas Pelayanan Kesehatan telah memenuhi standar akreditasi dan dinyatakan lulus :</p>
+      </div>
 
-    <div class="sertifikat-garis">
-      <hr>
-    </div>
-
-    <div class="sertifikat-tingkat tingkat">
-      <?= htmlspecialchars(strtoupper($s->status_akreditasi)) ?>
-    </div>
-
-    <div class="capayan desc">
-      Telah mengikuti pelatihan dengan hasil memuaskan
-    </div>
-
-    <div class="berlaku">
-      Berlaku hingga <?= date('d F Y', strtotime('+5 years', strtotime($s->tgl_surveior))) ?>
-    </div>
-
-    <div class="capayanimgparipurna">
-      <?php if (!empty($s->capayan_base64)) : ?>
-        <img src="<?= $s->capayan_base64 ?>" height="60" />
+       <div class="capayan">
+      <?php if ($s->status_akreditasi == 'Paripurna'): ?>
+        <img src="<?= $capayan_paripurna ?>" height="60" />
+      <?php elseif ($s->status_akreditasi == 'Utama'): ?>
+        <img src="<?= $capayan_utama ?>" height="60" />
+      <?php elseif ($s->status_akreditasi == 'Madya'): ?>
+        <img src="<?= $capayan_madya ?>" height="60" />
+      <?php elseif ($s->status_akreditasi == 'Dasar'): ?>
+        <img src="<?= $capayan_dasar ?>" height="60" />
       <?php endif; ?>
     </div>
 
-    <div class="ttdlembaga">
-      <p>Kepala Lembaga</p>
-      <img src="<?= $ttd_kepala_base64 ?>" width="100" />
-    </div>
+      <div class="berlaku">
+        <p class="desc">Masa Berlaku : <?= format_indo($s->tgl_surveior) ?> s.d <?= format_indo(date('Y-m-d', strtotime('+5 year', strtotime($s->tgl_surveior)))); ?></p>
+      </div>
 
-    <div class="ttddirjen">
-      <p>Dirjen Pelayanan Kesehatan</p>
-      <img src="<?= $ttd_dirjen_base64 ?>" width="100" />
-    </div>
-
+    </main>
   <?php } ?>
 </body>
 
