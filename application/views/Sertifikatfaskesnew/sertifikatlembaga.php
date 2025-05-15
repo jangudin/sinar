1cf7de9073e86f45841b19d7657227cf6fd37e0e
wahyu din <jangudin12345@gmail.com>
2025-05-15 00:51:13 -0700

    ganti user pass sinar tte

--- application/views/Sertifikatfaskesnew/sertifikatlembaga.php
+++ application/views/Sertifikatfaskesnew/sertifikatlembaga.php
@@ -234,7 +234,7 @@
         <p class="desc">Masa Berlaku : <?= format_indo($s->tgl_survei) ?> s.d <?= format_indo(date('Y-m-d', strtotime('+5 year', strtotime($s->tgl_survei)))); ?></p>
       </div>
 
-      <div class="logo-lembaga">
+      <div class="ttdlembaga">
         <img src="<?= $s->logo ?>" height="90" />
       </div>
 
