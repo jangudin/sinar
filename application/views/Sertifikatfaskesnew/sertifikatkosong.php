9b5f55a372539d176088c879e60f807152d01c77
wahyu din <jangudin12345@gmail.com>
2025-05-14 21:00:21 -0700

    ganti user pass sinar tte

--- application/views/Sertifikatfaskesnew/sertifikatkosong.php
+++ application/views/Sertifikatfaskesnew/sertifikatkosong.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html>
-<html lang="id">
+<!doctype html>
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
   <title>Sertifikat</title>
   <style>
     @page {
@@ -11,18 +12,18 @@
     body {
       font-family: 'Arial', sans-serif;
       margin-top: 180px;
-      margin-bottom: 0;
-      margin-left: 0;
-      margin-right: 0;
+      margin-bottom: 0cm;
+      margin-left: 0cm;
+      margin-right: 0cm;
       position: relative;
     }
 
     #watermark {
-      position: absolute;
+      position: fixed;
       bottom: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
+      width: 29.7cm;
+      height: 21cm;
       z-index: -1000;
     }
 
@@ -31,62 +32,150 @@
     .sertifikat-garis,
     .sertifikat-tingkat,
     .capayan,
-    .berlaku,
-    .capayanimg,
+    .capayanimgparipurna,
+    .capayanimgutama,
     .ttdlembaga,
-    .ttddirjen {
-      position: absolute;
+    .ttddirjen,
+    .berlaku,
+    .tglprn,
+    .tglsertifikat,
+    .tglsekarang,
+    .atasnama {
+      position: fixed;
       margin: auto;
       text-align: center;
+    }
+
+    .sertifikat-nomor {
+      top: 25%;
       width: 100%;
     }
 
-    .sertifikat-nomor { top: 25%; }
-    .sertifikat-nama { top: 30%; font-size: 24px; font-weight: bold; }
-    .sertifikat-garis { top: 33%; }
-    .sertifikat-tingkat { top: 46%; font-size: 28px; font-weight: bold; }
-    .capayan { top: 60%; font-size: 20px; }
-    .berlaku { top: 67%; font-size: 18px; }
+    .sertifikat-nama {
+      top: 30%;
+      left: 14%;
+      width: 800px;
+    }
 
-    .capayanimg { top: 52%; }
-    .ttdlembaga { top: 80%; left: 15%; text-align: left; }
-    .ttddirjen { top: 80%; left: 60%; text-align: left; }
+    .sertifikat-garis {
+      top: 33%;
+      left: 14%;
+      width: 800px;
+    }
+
+    .sertifikat-tingkat {
+      top: 46%;
+      width: 100%;
+    }
+
+    .capayan {
+      top: 60%;
+      width: 100%;
+    }
+
+    .berlaku {
+      top: 67%;
+      width: 100%;
+    }
+
+    .capayanimgparipurna,
+    .capayanimgutama {
+      top: 52%;
+      left: 25%;
+    }
+
+    .ttdlembaga {
+      top: 80%;
+      left: 15%;
+    }
+
+    .ttddirjen {
+      top: 80%;
+      left: 55%;
+    }
+
+    .title {
+      font-size: 17px;
+      font-weight: bold;
+    }
+
+    .desc {
+      font-size: 17px;
+    }
+
+    .tingkat {
+      font-size: 30px;
+      font-weight: bold;
+    }
   </style>
 </head>
+
 <body>
   <div id="watermark">
-    <img src="<?= base_url('assets/faskesbg/backgroundsertifikat.jpeg') ?>" width="100%" height="100%">
+    <img src="file:///var/www/html/sinar/assets/faskesbg/backgroundsertifikat.jpeg" width="100%" height="100%" />
   </div>
 
-  <?php foreach ($data as $s): ?>
-    <div class="sertifikat-nomor">Nomor: <?= $s->nomor_sertifikat ?? '....' ?></div>
-    <div class="sertifikat-nama"><?= strtoupper($s->nama_fasyankes) ?></div>
-    <div class="sertifikat-garis"><hr></div>
-    <div class="sertifikat-tingkat">TINGKAT <?= strtoupper($s->status_akreditasi) ?></div>
-    <div class="capayan">Telah memenuhi standar akreditasi</div>
-    <div class="berlaku">Berlaku: <?= format_indo($s->tgl_surveior) ?> s.d <?= format_indo(date('Y-m-d', strtotime('+5 years', strtotime($s->tgl_surveior)))) ?></div>
+  <?php foreach ($data as $s) { ?>
+    <div class="sertifikat-nomor">
+      <p style="font-size: 20px; color: red;">Nomor : <?= $s->nomor_sertifikat ?? '123/ABC/2024' ?></p>
+    </div>
 
-    <div class="capayanimg">
-      <?php if ($s->status_akreditasi == 'Paripurna'): ?>
-        <img src="<?= base_url('assets/faskessertif/capayan/paripurna.png') ?>" height="60">
-      <?php elseif ($s->status_akreditasi == 'Utama'): ?>
-        <img src="<?= base_url('assets/faskessertif/capayan/utama.png') ?>" height="60">
-      <?php elseif ($s->status_akreditasi == 'Madya'): ?>
-        <img src="<?= base_url('assets/faskessertif/capayan/madya.png') ?>" height="60">
-      <?php elseif ($s->status_akreditasi == 'Dasar'): ?>
-        <img src="<?= base_url('assets/faskessertif/capayan/dasar.png') ?>" height="60">
-      <?php endif; ?>
+    <div class="sertifikat-nama title">
+      <?= htmlspecialchars($s->nama_fasyankes) ?>
+    </div>
+
+    <div class="sertifikat-garis">
+      <hr>
+    </div>
+
+    <div class="sertifikat-tingkat tingkat">
+      <?= htmlspecialchars(strtoupper($s->status_akreditasi)) ?>
+    </div>
+
+    <div class="capayan desc">
+      Telah mengikuti pelatihan dengan hasil memuaskan
+    </div>
+
+    <div class="berlaku">
+      Berlaku hingga <?= date('d F Y', strtotime('+5 years', strtotime($s->tgl_surveior))) ?>
+    </div>
+
+    <div class="capayanimgparipurna">
+      <?php
+      $capayanPath = 'file:///var/www/html/sinar/assets/faskessertif/capayan/';
+      $imgCapayan = '';
+      switch ($s->status_akreditasi) {
+        case 'Paripurna':
+          $imgCapayan = $capayanPath . 'paripurna.png';
+          break;
+        case 'Utama':
+          $imgCapayan = $capayanPath . 'utama.png';
+          break;
+        case 'Madya':
+          $imgCapayan = $capayanPath . 'madya.png';
+          break;
+        case 'Dasar':
+          $imgCapayan = $capayanPath . 'dasar.png';
+          break;
+      }
+      if ($imgCapayan) {
+        echo '<img src="' . $imgCapayan . '" height="60">';
+      }
+      ?>
     </div>
 
     <div class="ttdlembaga">
       <p>Kepala Lembaga</p>
-      <img src="<?= base_url('assets/ttd/kepala.png') ?>" width="100">
+      <img src="file:///var/www/html/sinar/assets/ttd/kepala.png" width="100">
     </div>
 
     <div class="ttddirjen">
       <p>Dirjen Pelayanan Kesehatan</p>
-      <img src="<?= base_url('assets/ttd/dirjen.png') ?>" width="100">
+      <img src="file:///var/www/html/sinar/assets/ttd/dirjen.png" width="100">
     </div>
-  <?php endforeach; ?>
+
+  <?php } ?>
+
 </body>
+
 </html>
