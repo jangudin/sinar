2a33f37485b45391985584138cf79cc3a5532cf3
wahyu din <jangudin12345@gmail.com>
2025-05-14 20:40:37 -0700

    ganti user pass sinar tte

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Auth';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['filesertifikat/(:any)/(:any)'] = 'Mutu_fasyankes/filesertifikat/$1/$2';
