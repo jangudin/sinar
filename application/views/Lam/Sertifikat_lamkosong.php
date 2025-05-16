1ee9a1ead1dc0cae97009cd678b5545abcc99d49
wahyu din <jangudin12345@gmail.com>
2025-05-16 01:48:44 -0700

    tutup menu TPMD sementara

--- application/views/Lam/Sertifikat_lamkosong.php
+++ application/views/Lam/Sertifikat_lamkosong.php
@@ -177,13 +177,6 @@ body { margin: 0px; }
     </table><br><br>
     <?php } ?>
 
-    <table class="center" style="text-align: center;">
-        <tr>
-        <td style="font-size:25px; margin-top:20px;"> Mengetahui,</td>
-        </tr>
-        
-    </table><br><br>
-
 
 
 
