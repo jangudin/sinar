84cb50e20a9615a8b26cf32765e02dc97764e8c7
wahyu din <jangudin12345@gmail.com>
2025-05-15 23:59:39 -0700

    tutup menu TPMD sementara

--- application/views/lembaga/Sertifikat_kars.php
+++ application/views/lembaga/Sertifikat_kars.php
@@ -161,7 +161,7 @@
          <div class="sertifikat-tingkat">
             <p class="tingkat">TINGKAT KELULUSAN :</p>
             <?php if ($s->capayan == 'Paripurna'): ?>
-            <img src="<?= $paripurna ?>" alt="" height=100 class="capayanimgmadya" style="position: absolute; left: 200px;"></img>
+            <img src="<?= $paripurna ?>" alt="" height=100 class="capayanimgmadya"></img>
             <?php elseif ($s->capayan == 'Utama'): ?>
             <img src="<?= $utama ?>" alt="" height=100 class="capayanimgmadya" style="position: absolute; left: 200px;"></img>
             <?php elseif ($s->capayan == 'Madya'): ?>
