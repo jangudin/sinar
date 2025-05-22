c67ff77eb625347b654af22d2bbc08166aa484c8
wahyu din <jangudin12345@gmail.com>
2025-05-22 16:19:54 +0700

    uppadete laborat

--- application/controllers/AkreditasiNonRS.php
+++ application/controllers/AkreditasiNonRS.php
@@ -204,9 +204,9 @@ public function filesertifikatdir($id,$idp)
             $data['dir'] = $this->base64EncodeImage(FCPATH . 'assets/ttdfaskes/dirjen.png');
         }elseif($row->jenis_faskes === 'Unit Transfusi Darah'){
             $data['dir'] = $this->base64EncodeImage(FCPATH . 'assets/ttdfaskes/dirjen.png');
-        }elseif($row->jenis_faskes === 'Laboratorium Kesehatan' || $row->jenis_faskes === 'Laboratorium' && substr($row->kategoriFaskes, 0, 18) ==='Laboratorium Medis'){
+        }elseif($row->jenis_faskes === 'Laboratorium' && substr($row->kategoriFaskes, 0, 18) ==='Laboratorium Medis'){
             $data['dir'] = $this->base64EncodeImage(FCPATH . 'assets/ttdfaskes/dirjen.png');
-        }elseif($row->jenis_faskes === 'Laboratorium Kesehatan' || $row->jenis_faskes === 'Laboratorium' && substr($row->kategoriFaskes, 0, 18) !=='Laboratorium Medis'){
+        }elseif($row->jenis_faskes === 'Laboratorium' && substr($row->kategoriFaskes, 0, 18) !=='Laboratorium Medis'){
             $data['dir'] = $this->base64EncodeImage(FCPATH . 'assets/ttdfaskes/dirjendua.png');
         }
     }
