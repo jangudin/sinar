78acd891eaa81c36e7717b863004d7d2c5399466
wahyu din <jangudin12345@gmail.com>
2025-05-27 14:11:22 +0700

    uppadete laborat

--- application/views/Larsi/Sertifikat_larsikosong.php
+++ application/views/Larsi/Sertifikat_larsikosong.php
@@ -15,64 +15,71 @@
             background: url('data:image/png;base64,<?= base64_encode(file_get_contents(FCPATH . 'assets/sertifikat/larsi.png')) ?>') no-repeat center;
             background-size: 100% 100%;
             page-break-after: avoid;
+            height: 100%;
+            overflow: hidden; /* Prevent scrolling */
         }
         
         main {
             position: relative;
             width: 100%;
-            height: 100vh;
-            padding-top: 100px;
+            max-height: 42cm; /* A3 height */
+            padding-top: 80px;
             page-break-inside: avoid;
+            overflow: hidden;
         }
         
         .sertifikat-nomor {
-            position: relative;
-            width: 100%;
-            text-align: center;
-            margin-top: 200px;
+            margin-top: 180px;
         }
         
         .sertifikat-nama {
-            width: 800px;
-            margin: 30px auto;
-            text-align: center;
+            margin: 20px auto;
         }
         
         .sertifikat-tingkat {
-            width: 100%;
-            text-align: center;
-            margin: 20px auto;
+            margin: 15px auto;
         }
         
         .capayan-container {
             position: relative;
             width: 100%;
             text-align: center;
-            margin: 20px auto;
+            margin: 15px auto;
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
 
         .capayan-container img {
             display: block;
-            margin: 15px auto;
+            margin: 10px auto;
             width: 370px;
             height: 120px;
+            position: relative;
+            left: 50%;
+            transform: translateX(-50%);
         }
         
         .bsd {
-            width: 100%;
-            text-align: center;
-            margin: 25px auto;
+            margin: 20px auto;
         }
         
         .mengetahui {
             margin-left: 14%;
-            margin-top: 20px;
+            margin-top: 15px;
+            margin-bottom: 0;
         }
         
-        /* Typography */
-        h1, h2, h3, h4 {
-            margin: 8px 0;
-            padding: 0;
+        /* Keep all other existing styles but remove bottom margins from last elements */
+        .mengetahui h4 {
+            margin-bottom: 0;
+        }
+        
+        /* Force single page */
+        @media print {
+            .page-break {
+                display: none;
+            }
         }
     </style>
 </head>
