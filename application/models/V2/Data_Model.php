2351f79be2e06647f2d38ebf86d5c24197e6f780
wahyu din <jangudin12345@gmail.com>
2025-06-08 06:47:34 +0700

    perbaiki session

--- application/models/V2/Data_Model.php
+++ application/models/V2/Data_Model.php
@@ -51,11 +51,11 @@ class Data_Model extends CI_Model { // Match the filename case
             r.tanggal_terbit_sertifikat DESC
         LIMIT ? OFFSET ?";
             
-        // Execute query directly and get result
-        $result = $this->db->query($sql, [(int)$lem_id, (int)$limit, (int)$offset]);
+        // Bind parameters using query builder to ensure proper type casting
+        $this->db->query($sql, [(int)$lem_id, (int)$limit, (int)$offset]);
         
         return [
-            'data' => $result->result(),
+            'data' => $this->db->get()->result(),
             'total_rows' => $total_rows,
             'per_page' => $limit
         ];
