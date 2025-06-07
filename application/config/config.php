31d64141df1f1fd6c4b618367cb357b81fadd4ea
wahyu din <jangudin12345@gmail.com>
2025-06-08 03:39:49 +0700

    perbaiki session

--- application/config/config.php
+++ application/config/config.php
@@ -378,10 +378,10 @@ $config['encryption_key'] = 'ud1n';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;  // Set to NULL to use default system temp directory
+$config['sess_save_path'] = 'ci_sessions';  // table name
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -404,8 +404,8 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_secure']	= TRUE;
+$config['cookie_httponly'] 	= TRUE;
 
 /*
 |--------------------------------------------------------------------------
