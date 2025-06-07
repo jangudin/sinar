8b1f15f5088dcc4fba6dbc5f140c972235d440f1
wahyu din <jangudin12345@gmail.com>
2025-06-07 17:22:42 +0700

    update detail lembaga

--- application/controllers/V2/Home.php
+++ application/controllers/V2/Home.php
@@ -16,6 +16,8 @@ class Home extends CI_Controller {
 
 public function index() {
     $data['title'] = 'Dashboard - SINAR';
+    $this->load->view('V2/templates/header', $data);
     $this->load->view('V2/home/index', $data);
+    $this->load->view('V2/templates/footer');
 }
 }
