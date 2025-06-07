ce40124d10c18c46c3ae3e668f4400ef6d48914e
wahyu din <jangudin12345@gmail.com>
2025-06-07 16:05:44 +0700

    update detail lembaga

--- application/controllers/V2/Home.php
+++ application/controllers/V2/Home.php
@@ -1,12 +1,14 @@
 <?php
-<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
     
     public function __construct() {
         parent::__construct();
-        // Check if user is logged in
+        $this->load->library(['session']);
+        $this->load->helper(['url']);
+        
+        // Check authentication
         if($this->session->userdata('status') != "login") {
             redirect('V2/Login');
         }
@@ -14,7 +16,7 @@ class Home extends CI_Controller {
 
     public function index() {
         $data['title'] = 'Dashboard - SINAR';
-        // Add any additional data needed for the dashboard
-        $this->load->view('V2/home/index', $data);
+        $data['content'] = 'V2/home/index';
+        $this->load->view('V2/templates/index', $data);
     }
 }
