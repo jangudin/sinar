847321496ad2dd427324de0a11c0bfcde0f01aee
wahyu din <jangudin12345@gmail.com>
2025-06-08 03:10:28 +0700

    update detail lembaga

--- application/config/config.php
+++ application/config/config.php
@@ -325,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '5d05a33e7e95f479962b1e66ad6b1f84';
+$config['encryption_key'] = 'ud1n';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,10 +381,10 @@ $config['encryption_key'] = '5d05a33e7e95f479962b1e66ad6b1f84';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = str_replace('\\', '/', FCPATH . 'writable/sessions'); // Changed path
-$config['sess_match_ip'] = FALSE;
+$config['sess_save_path'] = 'ci_sessions';
+$config['sess_match_ip'] = TRUE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
