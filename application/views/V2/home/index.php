000b9ceee83eae17a37f77748a10937818d0cdaf
wahyu din <jangudin12345@gmail.com>
2025-06-07 18:04:49 +0700

    update detail lembaga

--- application/views/V2/home/index.php
+++ application/views/V2/home/index.php
@@ -128,7 +128,8 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
+            <!-- All navigation moved to right -->
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link active" href="#"><i class="fas fa-home"></i> Dashboard</a>
                 </li>
@@ -138,9 +139,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="fas fa-file-signature"></i> TTE</a>
                 </li>
-            </ul>
-            <!-- Moved to right -->
-            <ul class="navbar-nav ms-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown">
                         <img src="<?= base_url('assets/temp/img/loginlg1.png') ?>" alt="User" class="rounded-circle me-2" style="width: 32px; height: 32px;">
