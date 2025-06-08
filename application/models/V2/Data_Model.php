c5f284d23715bd7816840c9023709ca2aeca9ba7
wahyu din <jangudin12345@gmail.com>
2025-06-08 12:50:23 +0700

    v2

--- application/models/V2/Data_Model.php
+++ application/models/V2/Data_Model.php
@@ -9,7 +9,7 @@ class Data_Model extends CI_Model { // Match the filename case
         $limit = (int)$limit;
         $offset = (int)$offset;
 
-        // Get total count first with a separate query
+        // First get the total count
         $count_sql = "SELECT COUNT(DISTINCT r.id) as total 
             FROM db_akreditasi.rekomendasi r
             INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
@@ -21,8 +21,8 @@ class Data_Model extends CI_Model { // Match the filename case
 
         $total_rows = $this->db->query($count_sql, [$lem_id])->row()->total;
 
-        // Main query for data
-        $sql = "SELECT DISTINCT
+        // Get the actual data
+        $sql = "SELECT 
             r.id,
             r.no_sertifikat,
             ps.kode_rs AS kodeRS,
@@ -37,19 +37,18 @@ class Data_Model extends CI_Model { // Match the filename case
             DATE_FORMAT(r.tanggal_terbit_sertifikat, '%d-%m-%Y') as tgl_terbit,
             DATE_FORMAT(r.tanggal_kadaluarsa_sertifikat, '%d-%m-%Y') as tgl_kadaluarsa
         FROM db_akreditasi.rekomendasi r
-            INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
-            INNER JOIN db_akreditasi.pengajuan_survei ps ON ps.id = s.pengajuan_survei_id
-            INNER JOIN db_fasyankes.`data` d ON d.Propinsi = ps.kode_rs
-            LEFT JOIN db_akreditasi.Sertifikat_progres1 sp ON r.id = sp.id_rekomendasi
+        INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
+        INNER JOIN db_akreditasi.pengajuan_survei ps ON ps.id = s.pengajuan_survei_id
+        INNER JOIN db_fasyankes.`data` d ON d.Propinsi = ps.kode_rs
+        LEFT JOIN db_akreditasi.Sertifikat_progres1 sp ON r.id = sp.id_rekomendasi
         WHERE ps.lembaga_akreditasi_id = ?
-            AND r.tanggal_surat_pengajuan_sertifikat > '2022-12-08'
-            AND (sp.lembaga IS NULL OR sp.lembaga = '0')
-            AND r.tanggal_terbit_sertifikat IS NOT NULL 
-            AND r.tanggal_kadaluarsa_sertifikat IS NOT NULL
+        AND r.tanggal_surat_pengajuan_sertifikat > '2022-12-08'
+        AND (sp.lembaga IS NULL OR sp.lembaga = '0')
+        AND r.tanggal_terbit_sertifikat IS NOT NULL 
+        AND r.tanggal_kadaluarsa_sertifikat IS NOT NULL
         ORDER BY r.tanggal_terbit_sertifikat DESC
         LIMIT ? OFFSET ?";
 
-        // Execute the main query
         $result = $this->db->query($sql, [$lem_id, $limit, $offset]);
 
         return [
@@ -65,8 +64,21 @@ class Data_Model extends CI_Model { // Match the filename case
         $limit = (int)$limit;
         $offset = (int)$offset;
 
-        // Main query for completed TTE
-        $sql = "SELECT DISTINCT
+        // First get total count with separate query
+        $count_sql = "SELECT COUNT(DISTINCT r.id) as total 
+            FROM db_akreditasi.rekomendasi r
+            INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
+            INNER JOIN db_akreditasi.pengajuan_survei ps ON ps.id = s.pengajuan_survei_id
+            INNER JOIN db_akreditasi.Sertifikat_progres1 sp ON r.id = sp.id_rekomendasi
+            WHERE ps.lembaga_akreditasi_id = ?
+            AND sp.lembaga = '1'
+            AND sp.mutu = '1'
+            AND sp.dirjen = '1'";
+
+        $total_rows = $this->db->query($count_sql, [$lem_id])->row()->total;
+
+        // Main query for data
+        $sql = "SELECT 
             r.id,
             r.no_sertifikat,
             ps.kode_rs AS kodeRS,
@@ -82,26 +94,18 @@ class Data_Model extends CI_Model { // Match the filename case
             DATE_FORMAT(sp.tgl_dibuat_dirjen, '%d-%m-%Y') as tgl_dirjen,
             DATE_FORMAT(r.tanggal_terbit_sertifikat, '%d-%m-%Y') as tgl_terbit,
             DATE_FORMAT(r.tanggal_kadaluarsa_sertifikat, '%d-%m-%Y') as tgl_kadaluarsa
-        FROM
-            db_akreditasi.rekomendasi r
-            INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
-            INNER JOIN db_akreditasi.pengajuan_survei ps ON ps.id = s.pengajuan_survei_id
-            INNER JOIN db_fasyankes.`data` d ON d.Propinsi = ps.kode_rs
-            INNER JOIN db_akreditasi.Sertifikat_progres1 sp ON r.id = sp.id_rekomendasi
-        WHERE
-            ps.lembaga_akreditasi_id = ?
-            AND sp.lembaga = '1'
-            AND sp.mutu = '1'
-            AND sp.dirjen = '1'
-        ORDER BY 
-            sp.tgl_dibuat_dirjen DESC
+        FROM db_akreditasi.rekomendasi r
+        INNER JOIN db_akreditasi.survei s ON s.id = r.survei_id
+        INNER JOIN db_akreditasi.pengajuan_survei ps ON ps.id = s.pengajuan_survei_id
+        INNER JOIN db_fasyankes.`data` d ON d.Propinsi = ps.kode_rs
+        INNER JOIN db_akreditasi.Sertifikat_progres1 sp ON r.id = sp.id_rekomendasi
+        WHERE ps.lembaga_akreditasi_id = ?
+        AND sp.lembaga = '1'
+        AND sp.mutu = '1'
+        AND sp.dirjen = '1'
+        ORDER BY sp.tgl_dibuat_dirjen DESC
         LIMIT ? OFFSET ?";
 
-        // Get total rows count
-        $count_sql = str_replace('SELECT DISTINCT', 'SELECT COUNT(DISTINCT r.id) as total', 
-                               substr($sql, 0, strpos($sql, 'ORDER BY')));
-        
-        $total_rows = $this->db->query($count_sql, [$lem_id])->row()->total;
         $result = $this->db->query($sql, [$lem_id, $limit, $offset]);
 
         return [
